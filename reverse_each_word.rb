969f92f911df4080a3c1cd014a6bb7bbc8ab4651
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-08-04 15:24:37 -0400

    done

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,12 @@
 # Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+    words = sentence.split
+
+    reversed_words = words.map { |word| word.reverse }
+
+    reversed_words.join(" ")
+end
+
+puts reverse_each_word("Hello there, and how are you?")
